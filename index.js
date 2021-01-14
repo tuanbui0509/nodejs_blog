19afdaccec0dd7cd69184b675ba14a884a1e1c4c
tuanbui0509 <tuanbui0509@gmail.com>
2021-01-14 21:49:51 +0700

    add npm morgan library

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
     res.send('Hello World i love my earth!')
 })
 // 127.0.0.1 - localhost
