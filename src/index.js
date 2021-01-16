262ec8bb6f7ce63635db286de55d5bd063fcfa62
tuanbui0509 <tuanbui0509@gmail.com>
2021-01-16 17:07:38 +0700

    add handlebars search how to get and post

--- src/index.js
+++ src/index.js
@@ -9,18 +9,34 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 // Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set("views", path.join(__dirname, "resources/views"));
 // console.log(path.join(__dirname, "resources/views "));
+// use to midlleware form data to server
+app.use(express.urlencoded({
+    extended : true
+}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 app.get('/news', (req, res) => {
+   console.log(req.query.q);
         res.render('news');
+    })
+
+    app.get('/search', (req, res) => {
+       
+        res.render('search');
+    })
+    app.post('/search', (req, res) => {
+        console.log(req.body)
+        res.render('search');
     })
     // 127.0.0.1 - localhost
 
